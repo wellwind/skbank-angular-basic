798a206ad80c0cc83a467bff0e6db70f65a4359e
黃升煌 Mike <mike@miniasp.com>
2018-07-20 11:36:16 +0800

    搬移 HeaderComponent & 練習binding的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '[新光銀行]';
+  title = '[Hi!新光銀行]';
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title = 'The Will Will Web';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享!';
+  url = 'http://blog.miniasp.com/';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
